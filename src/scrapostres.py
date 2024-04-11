66fe76f849f43c31e262856beb53881690432325
natashacarola <natasha.arias@7puentes.com>
2024-04-11 11:36:07 -0300

    function scrap_recipe modified and add new function tryExcept

--- src/scrapostres.py
+++ src/scrapostres.py
@@ -23,6 +23,16 @@ def parse_html(page):
     result = request_page(page)
     return html.fromstring(result.content)
 
+def tryExcept(page,data,index,boolean):
+    try:
+        if(boolean):
+            return  page.xpath(data)[index]
+        else:
+            return page.xpath(data)
+    except IndexError as e:
+        return None
+
+
 def scrap_category(category_html, connector):
     recipes = category_html.xpath("//div[contains(@class,'content')]//a[contains(@class,'title')]/@href")
     for recipe in recipes:
@@ -36,28 +46,23 @@ def scrap_category(category_html, connector):
         return
 
 
-def scrap_recipe(recipe, connection):
+def scrap_recipe(recipe, connector):
     recipe_html = parse_html(recipe)
-    try:
-        title = recipe_html.xpath("//header[contains(@class,'recipes')]//h2[contains(@class,'title')]/text()")[0]
-        # ingredients = recipe_html.xpath("//div[contains(@class,'ingredients')]//ul")[0]
-        ingredients = ', '.join([i.text for i in recipe_html.xpath("//div[contains(@class,'ingredients')]//ul/li") if i.text is not None])
-    except IndexError as e:
-        title = None
-        ingredients = None
-    instructions = recipe_html.xpath("//div[contains(@class,'instructions')]//text()")
-    category = recipe_html.xpath("//div[contains(@class,'details')]/span[contains(@class,'category')]")
-    total_time = recipe_html.xpath("//span[contains(@class,'total')]/text()")
-    total_time_text = "".join(total_time).strip()
-    prep_time = recipe_html.xpath("//span[contains(@class,'prep')]/text()")
-    # la url es recipe
-    posted_date = recipe_html.xpath("//time[contains(@class,'entry-time')]/text()")
-    posted_date_text = "".join(posted_date).strip()
-    updated_date = recipe_html.xpath("//time[contains(@class,'entry-modified')]/text()")
-    updated_date_text = "".join(updated_date).strip()
-    cuisine = recipe_html.xpath("//span[contains(@class,'tasty-recipes-cuisine')]/text()")
-    #cuisine_text = cuisine[0].strip()
+    #ingredients = ', '.join([i.text for i in recipe_html.xpath("//div[contains(@class,'ingredients')]//ul/li") if i.text is not None])
+    #ingredients = tryExcept(recipe_html,"//div[contains(@class,'ingredients')]//ul",0,False)
     hearts = 0
+    recipe_html = parse_html(recipe)
+    title = tryExcept(recipe_html, "//header[contains(@class,'recipes')]//h2[contains(@class,'title')]/text()", 0, True)
+    instructions = tryExcept(recipe_html,"//div[contains(@class,'instructions')]//text()",0,False)
+    instructions_text = ''.join(instructions).strip()
+    category = tryExcept(recipe_html,"//span[contains(@class,'tasty-recipes-category')]/text()",0,True)
+    total_time = tryExcept(recipe_html,"//span[contains(@class,'tasty-recipes-total')]/text()",0,True)
+    prep_time = tryExcept(recipe_html,"//span[contains(@class,'tasty-recipes-prep')]/text()",0,True)
+    posted_date = tryExcept(recipe_html,"//time[contains(@class,'entry-time')]/text()",0,True)
+    update_date = tryExcept(recipe_html,"//time[contains(@class,'entry-modified')]/text()",0,True)
+    cuisine  = tryExcept(recipe_html,"//span[contains(@class,'tasty-recipes-cuisine')]/text()",0,True)
+
+    # la url es recipe
 
     # TODO: Fix this
     # category = ', '.join([i.text for i in recipe_html.xpath("//div[contains(@class,'details')]/span[contains(@class,'category')]") if i.text is not None])
@@ -69,7 +74,7 @@ def scrap_recipe(recipe, connection):
     #     INSERT INTO Recipes (RecipeID, Name, Category, Ingredients, RecipeURL, PostedDate, UpdatedDate, Hearts, PrepTime, TotalTime, Cuisine)
     #     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
     #     """
-    
+
     # execute_insert_query(insert_query, connection, (None, title, category, ingredients, recipe, posted_date_text, updated_date_text, hearts, prep_time, total_time_text, cuisine))
 
 def has_next_page(page_html):
@@ -95,7 +100,7 @@ def main():
     except (Exception, Error) as error:
         print("Error while connecting to PostgreSQL", error)
         return
-    
+
     home_page = 'https://www.sweetestmenu.com/'
     home_html = parse_html(home_page)
 
@@ -104,7 +109,7 @@ def main():
     for category in categories:
         category_html = parse_html(category)
         scrap_category(category_html, connection)
-    
+
     connection.close()
 
 
